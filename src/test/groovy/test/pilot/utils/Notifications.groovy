eb670d4ba3e00c6a1d6dd45ae16639e2ef98fdab
Murali Ramanujam <rmkrishnann@gmail.com>
2021-04-22 23:11:14 +0100

    Update Notifications.groovy

--- src/test/groovy/test/pilot/utils/Notifications.groovy
+++ src/test/groovy/test/pilot/utils/Notifications.groovy
@@ -2,9 +2,9 @@ package test.pilot.utils
 
 class Notifications {
 
-    static final String CONTACT_PREFERENCE_SUCCESS_MESSGAGE = "",
-                        CONTACT_PREFERENCE_SUCCESS_TITLE = "",
-                        CONTACT_PREFERENCE_NUMBER = ""
+    static final String CONTACT_SUCCESS_MESSGAGE = "",
+                        CONTACT_SUCCESS_TITLE = "",
+                        CONTACT_NUMBER = ""
 
 
 
